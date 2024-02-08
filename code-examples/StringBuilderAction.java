b0a105d22362f46fcb94f1583484d86bced853f4
Ambrose Otundo <48138192+AmbroseOtundo@users.noreply.github.com>
2024-02-07 23:56:45 -0500

    HOT FIX: Codepoints in java. Char and stringbuilders

---
+++ code-examples/StringBuilderAction.java
@@ -0,0 +1,31 @@
+public class StringBuilderAction{
+    public static void main(String[] args) {
+        // Construct an empty StringBuilder
+        StringBuilder builder = new StringBuilder();
+//Use is to save memory
+        // Add individual characters
+        char ch = 'H';
+        builder.append(ch);
+        System.out.println("After appending '" + ch + "': " + builder.toString());
+
+        // Add a string
+        String str = "ello";
+        builder.append(str);
+        System.out.println("After appending \"" + str + "\": " + builder.toString());
+
+        // Add more characters
+        builder.append(' ');
+        System.out.println("After appending a space: " + builder.toString());
+
+        // Add another string
+        String greeting = "World!";
+        builder.append(greeting);
+        System.out.println("After appending \"" + greeting + "\": " + builder.toString());
+
+        // Convert StringBuilder to String
+        String completedString = builder.toString();
+
+        // Print the final string
+        System.out.println("Completed String: " + completedString);
+    }
+}
---
+++ code-examples/codepoint.java
@@ -0,0 +1,14 @@
+public class codepoint {
+    public static void main(String[] args){
+        //CODE POINTS and CODE UNITS
+        // Count of chars in a word/phrase == string
+    // The length method is used
+        String greetings = "Hi there";
+        int length = greetings.length();
+        System.out.println("The length in this is: " + length);
+    //Using Code point to get the number of codepoints
+        int cpCount = greetings.codePointCount(0, greetings.length());
+        System.out.println("The codepoint is : " + cpCount);
+
+    }
+}
